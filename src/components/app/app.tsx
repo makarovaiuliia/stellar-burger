15d5c5f6d6a7b95376d71fbde57cf7c584d3f175
Iuliia Makarova <iuliia.makarova.au@gmail.com>
2024-03-29 12:07:05 +0100

    feat: routing settings
    Created routing setting for main app pages.

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,65 @@
-import { ConstructorPage } from '@pages';
+import {
+  ConstructorPage,
+  Feed,
+  ForgotPassword,
+  Login,
+  NotFound404,
+  Profile,
+  ProfileOrders,
+  Register,
+  ResetPassword
+} from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
 
-import { AppHeader } from '@components';
+import {
+  AppHeader,
+  FeedInfo,
+  IngredientDetails,
+  Modal,
+  OrderInfo
+} from '@components';
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => (
   <div className={styles.app}>
     <AppHeader />
-    <ConstructorPage />
+    <Routes>
+      <Route path='/' element={<ConstructorPage />} />
+      <Route path='/feed' element={<Feed />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/forgot-password' element={<ForgotPassword />} />
+      <Route path='/reset-password' element={<ResetPassword />} />
+      <Route path='/profile' element={<Profile />} />
+      <Route path='/profile/orders' element={<ProfileOrders />} />
+      <Route path='*' element={<NotFound404 />} />
+    </Routes>
+
+    <Routes>
+      <Route
+        path='/feed/:number'
+        element={
+          <Modal title='hello' children={<FeedInfo />} onClose={() => {}} />
+        }
+      />
+      <Route
+        path='/ingredients/:id'
+        element={
+          <Modal
+            title='Детали ингредиента'
+            children={<IngredientDetails />}
+            onClose={() => {}}
+          />
+        }
+      />
+      <Route
+        path='/profile/orders/:number'
+        element={
+          <Modal title='hello' children={<OrderInfo />} onClose={() => {}} />
+        }
+      />
+    </Routes>
   </div>
 );
 
