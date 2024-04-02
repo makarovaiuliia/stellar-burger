c67e7cbf71690ecfb83ad3aea538d47ca366df60
Iuliia Makarova <iuliia.makarova.au@gmail.com>
2024-04-02 16:41:04 +0200

    feat: modal functionality for ingredients

--- src/components/burger-ingredients/burger-ingredients.tsx
+++ src/components/burger-ingredients/burger-ingredients.tsx
@@ -4,7 +4,7 @@ import { useInView } from 'react-intersection-observer';
 import { TTabMode } from '@utils-types';
 import { BurgerIngredientsUI } from '../ui/burger-ingredients';
 import { useSelector } from 'react-redux';
-import { RootState, useDispatch } from '../../services/store';
+import { useDispatch } from '../../services/store';
 import { getIngredients, ingredients } from '../../services/ingredientSlice';
 
 export const BurgerIngredients: FC = () => {
@@ -59,8 +59,6 @@ export const BurgerIngredients: FC = () => {
     if (tab === 'sauce')
       titleSaucesRef.current?.scrollIntoView({ behavior: 'smooth' });
   };
-
-  // return null;
 
   return (
     <BurgerIngredientsUI
