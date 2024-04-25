81a0e6aed3d0590be9273dc0d62451c6d761fe0a
Iuliia Makarova <iuliia.makarova.au@gmail.com>
2024-04-25 12:01:01 +0300

    fix: tests cypress

--- cypress/e2e/constructor.cy.ts
+++ cypress/e2e/constructor.cy.ts
@@ -1,6 +1,6 @@
 describe('Ingredient Constructor Tests', () => {
   beforeEach(() => {
-    cy.intercept('GET', 'http://localhost:4000/ingredients', {
+    cy.intercept('GET', 'https://norma.nomoreparties.space/api/ingredients', {
       fixture: 'ingredients.json'
     }).as('getIngredients');
 
@@ -17,7 +17,7 @@ describe('Ingredient Constructor Tests', () => {
     cy.get('.constructor-element__row').should('contain', 'булка');
   });
 
-  it.only('adds an ingredient to the constructor', () => {
+  it('adds an ingredient to the constructor', () => {
     cy.contains('.text', 'Биокотлета')
       .parents('li')
       .within(() => {
@@ -33,37 +33,4 @@ describe('Ingredient Constructor Tests', () => {
     cy.get('button[data-info="modal-close"]').click();
     cy.get('div[data-info="ingredient-modal"]').should('not.exist');
   });
-
-  // it('Order creation tests', () => {
-  //   cy.visit(`http://localhost:4000/login`);
-
-  //   cy.get('.input__textfield[type="email"]').type('makarova@gmail.com');
-  //   cy.get('.input__textfield[type="password"]').type('qwerty');
-  //   cy.get('button[type="submit"]').click();
-
-  //   cy.visit('http://localhost:4000');
-
-  //   cy.contains('.text', 'булка')
-  //     .parents('li')
-  //     .within(() => {
-  //       cy.get('button').contains('Добавить').click();
-  //     });
-
-  //   cy.contains('.text', 'Биокотлета')
-  //     .parents('li')
-  //     .within(() => {
-  //       cy.get('button').contains('Добавить').click();
-  //     });
-
-  //   cy.contains('.text', 'Филе')
-  //     .parents('li')
-  //     .within(() => {
-  //       cy.get('button').contains('Добавить').click();
-  //     });
-
-  //   cy.get('button[data-info="order-button"]').click(); // вот тут меня переводит на страницу логина и поэтому тест не работает
-  //   // cy.get('div[data-info="order-modal"]', { timeout: 50000 }).should(
-  //   //   'be.visible'
-  //   // );
-  // });
 });
--- cypress/e2e/orderCreation.cy.ts
+++ cypress/e2e/orderCreation.cy.ts
@@ -1,72 +1,34 @@
-// describe('Order creation tests', () => {
-//   beforeEach(() => {
-//     cy.intercept('GET', 'api/ingredients', {
-//       fixture: 'ingredients.json'
-//     }).as('getIngredients');
+describe('Order creation tests', () => {
+  it('logs in and creates an order', () => {
+    cy.visit('http://localhost:4000/login');
+    cy.get('.input__textfield[type="email"]').type('makarova@gmail.com');
+    cy.get('.input__textfield[type="password"]').type('qwerty');
+    cy.get('button[type="submit"]').click();
 
-//     cy.intercept('POST', 'api/auth/login', {
-//       fixture: 'loginResponse.json'
-//     }).as('login');
+    cy.visit('http://localhost:4000');
 
-//     cy.intercept('GET', 'api/orders/*', { fixture: 'orderResponse.json' }).as(
-//       'orderData'
-//     );
+    cy.contains('.text', 'булка')
+      .parents('li')
+      .within(() => {
+        cy.get('button').contains('Добавить').click();
+      });
 
-//     cy.intercept('POST', '/api/orders/create', {
-//       fixture: 'createOrderResponse.json'
-//     }).as('createOrder');
+    cy.contains('.text', 'Биокотлета')
+      .parents('li')
+      .within(() => {
+        cy.get('button').contains('Добавить').click();
+      });
 
-//     localStorage.setItem('auth_token', '12345');
+    cy.contains('.text', 'Филе')
+      .parents('li')
+      .within(() => {
+        cy.get('button').contains('Добавить').click();
+      });
 
-//     cy.visit('http://localhost:4000');
+    cy.get('button[data-info="order-button"]').click();
 
-//   });
-
-//   it('logs in and creates an order', () => {
-//     cy.visit('http://localhost:4000/login');
-//     cy.get('.input__textfield[type="email"]').type('makarova@gmail.com');
-//     cy.get('.input__textfield[type="password"]').type('qwerty');
-//     cy.get('button[type="submit"]').click();
-
-//     cy.wait('@login');
-
-//     cy.visit('http://localhost:4000');
-
-//     // Add items to the burger
-//     cy.contains('.text', 'булка')
-//       .parents('li')
-//       .within(() => {
-//         cy.get('button').contains('Добавить').click();
-//       });
-
-//     cy.contains('.text', 'Биокотлета')
-//       .parents('li')
-//       .within(() => {
-//         cy.get('button').contains('Добавить').click();
-//       });
-
-//     cy.contains('.text', 'Филе')
-//       .parents('li')
-//       .within(() => {
-//         cy.get('button').contains('Добавить').click();
-//       });
-
-//     // Place the order
-//     cy.get('button[data-info="order-button"]').click();
-//     cy.wait('@createOrder');
-
-//     // Check if the modal with the order appears
-//     cy.get('div[data-info="order-modal"]', { timeout: 50000 }).should(
-//       'be.visible'
-//     );
-//     // Check the order number
-//     cy.get('div[data-info="order-modal"]').contains('Order number: 12345');
-
-//     // Close the modal and check if it closed successfully
-//     cy.get('button[data-info="close-modal"]').click();
-//     cy.get('div[data-info="order-modal"]').should('not.exist');
-
-//     // Check if the constructor is empty
-//     cy.get('.constructor').should('not.have.descendants', 'li');
-//   });
-// });
+    cy.get('div[data-info="order-modal"]', { timeout: 50000 }).should(
+      'be.visible'
+    );
+  });
+});
